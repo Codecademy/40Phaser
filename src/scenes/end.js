bf2613dcca55b3570699e39a5d6e5b48b5b62de9
Gonzalo Reyes <gonzaloreyesllauca@gmail.com>
2019-07-01 13:22:10 -0400

    Move dom element creation to end.js

--- src/scenes/end.js
+++ src/scenes/end.js
@@ -1,4 +1,4 @@
-import elements from "../elements.js";
+import options from "../options.js";
 
 export default class EndScene extends Phaser.Scene {
     constructor() {
@@ -8,16 +8,63 @@ export default class EndScene extends Phaser.Scene {
     preload() {}
 
     create(data) {
-        elements.appEnd.style.visibility = "visible";
-        elements.appEnd.style.display = "flex";
-        elements.appEnd.style.marginTop = "10%";
-        elements.endScoreDisplay.innerHTML = `Score: ${data.score}`;
+        this.add.text(options.windowWidth / 2 - 110, 60, `Score: ${data.score}`, {
+            fontFamily: "sans-serif",
+            fontSize: "48px",
+            fontStyle: "bold",
+            color: "#000",
+            align: "center",
+        });
 
-        elements.buttonGameReplay.onclick = () => {
-            elements.appEnd.style.visibility = "invisible";
-            elements.appEnd.style.display = "none";
+        this.add.text(options.windowWidth / 2 - 35, options.windowHeight / 4, "So Close!", {
+            fontFamily: "sans-serif",
+            fontSize: "17px",
+            fill: "#000",
+        });
+
+        const gameReplay = this.add.rectangle(
+            options.windowWidth / 2,
+            options.windowHeight / 3 + 25,
+            210,
+            40,
+            0x6400e4,
+        );
+
+        const buildYourOwn = this.add.rectangle(
+            options.windowWidth / 2,
+            options.windowHeight / 2 - 20,
+            210,
+            40,
+            0xf9f9f9,
+        );
+
+        gameReplay.setInteractive();
+        buildYourOwn.setInteractive();
+
+        gameReplay.on("pointerup", () => {
             this.scene.stop("EndScene");
             this.scene.start("GameScene");
-        };
+        });
+
+        buildYourOwn.on("pointerup", () => {
+            window.location.href("https://www.codecademy.com/learn/learn-phaser");
+        });
+
+        this.add.text(options.windowWidth / 2 - 40, options.windowHeight / 3 + 15, "Play Again", {
+            fontFamily: "sans-serif",
+            fontSize: "17px",
+            fill: "#fff",
+        });
+
+        this.add.text(
+            options.windowWidth / 2 - 55,
+            options.windowHeight / 2 - 40,
+            "Build your own",
+            {
+                fontFamily: "sans-serif",
+                fontSize: "17px",
+                fill: "#6400e4",
+            },
+        );
     }
 }
