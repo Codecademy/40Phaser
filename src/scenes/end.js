d12443a830e2f066deaa0d0b30cd16f920690cfa
Josh Goldberg <josh@codecademy.com>
2019-12-26 15:11:41 -0500

    Adjusted font sizes and text for mobile friendliness
    Used this.game.config.(height/width) instead of options.window(Height/Width) so the game can be made with smaller viewports and still have text centered. Good stuff.

--- src/scenes/end.js
+++ src/scenes/end.js
@@ -12,37 +12,37 @@ export default class EndScene extends Phaser.Scene {
     }
 
     createDOMElements({ score, highscore }) {
-        this.add.text(options.windowWidth / 2 - 110, 50, `Score: ${score}`, {
+        this.add.text(this.game.config.width / 2 - 110, 50, `Score: ${score}`, {
             fontFamily: options.fontFamily,
-            fontSize: options.largeFontSize,
+            fontSize: options.extraLargeFontSize,
             fontStyle: "bold",
             color: options.blackText,
             align: "center",
         });
 
-        this.add.text(options.windowWidth / 2 - 80, 110, `Highscore: ${highscore}`, {
+        this.add.text(this.game.config.width / 2 - 80, 110, `Highscore: ${highscore}`, {
             fontFamily: options.fontFamily,
             fontSize: options.mediumFontSize,
             fill: options.blackText,
         });
 
-        this.add.text(options.windowWidth / 2 - 35, options.windowHeight / 4, "So Close!", {
+        this.add.text(this.game.config.width / 2 - 35, this.game.config.height / 4, "So Close!", {
             fontFamily: options.fontFamily,
             fontSize: options.smallFontSize,
             fill: options.blackText,
         });
 
         const gameReplay = this.add.rectangle(
-            options.windowWidth / 2,
-            options.windowHeight / 3 + 25,
+            this.game.config.width / 2,
+            this.game.config.height / 3 + 25,
             210,
             40,
             options.purpleBox,
         );
 
         const buildYourOwn = this.add.rectangle(
-            options.windowWidth / 2,
-            options.windowHeight / 2 - 20,
+            this.game.config.width / 2,
+            this.game.config.height / 2 - 20,
             210,
             40,
             options.backgroundColor,
@@ -60,15 +60,20 @@ export default class EndScene extends Phaser.Scene {
             window.location.href = "https://www.codecademy.com/learn/learn-phaser";
         });
 
-        this.add.text(options.windowWidth / 2 - 40, options.windowHeight / 3 + 15, "Play Again", {
-            fontFamily: options.fontFamily,
-            fontSize: options.smallFontSize,
-            fill: options.whiteText,
-        });
+        this.add.text(
+            this.game.config.width / 2 - 40,
+            this.game.config.height / 3 + 15,
+            "Play Again",
+            {
+                fontFamily: options.fontFamily,
+                fontSize: options.smallFontSize,
+                fill: options.whiteText,
+            },
+        );
 
         this.add.text(
-            options.windowWidth / 2 - 55,
-            options.windowHeight / 2 - 30,
+            this.game.config.width / 2 - 55,
+            this.game.config.height / 2 - 30,
             "Build your own",
             {
                 fontFamily: options.fontFamily,
